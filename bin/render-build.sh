5f2eb0b528b9b176d10039a304fadaf91d564f8a
sugimori kouta <kota.1412@icloud.com>
2024-04-05 00:37:50 +0900

    連打

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,5 +2,6 @@ set -o errexit
 bundle install;
 bundle exec rake assets:precompile;
 bundle exec rake assets:clean;
+bundle exec rails db:migrate:reset;
 bundle exec rake db:migrate;
 bundle exec rake db:seed;
